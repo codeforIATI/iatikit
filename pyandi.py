77a85c75c05a9b439177403f42089559e3613500
Andy Lulham <a.lulham@gmail.com>
2018-09-14 12:10:42 +0100

    Add an __exists modifier

--- pyandi.py
+++ pyandi.py
@@ -126,12 +126,21 @@ class QueryBuilder():
                     op=op,
                     value=value,
                 )
-            elif op != 'eq':
-                raise Exception
-            return '{expr} = "{value}"'.format(
-                expr=self.get(),
-                value=value,
-            )
+            elif op == 'exists':
+                if value is False:
+                    subop = '= 0'
+                else:
+                    subop = '!= 0'
+                return 'count({expr}) {subop}'.format(
+                    expr=self.get(),
+                    subop=subop,
+                )
+            elif op == 'eq':
+                return '{expr} = "{value}"'.format(
+                    expr=self.get(),
+                    value=value,
+                )
+            raise Exception
 
     def __init__(self, version):
         self._version = version
