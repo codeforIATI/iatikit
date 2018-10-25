d017652be90dc7967053d77c98611277021fae3d
Andy Lulham <a.lulham@gmail.com>
2018-10-25 12:25:53 +0100

    More codelist stuff

--- pyandi/codelists.py
+++ pyandi/codelists.py
@@ -1,50 +1,91 @@
+from collections import UserDict
 import json
-from glob import glob
-from os.path import join
+from os import listdir
+from os.path import exists, join, splitext
 
 
-class Codelist:
-    def __init__(self, name):
-        self.path = join('__pyandicache__', 'codelists')
-        files = glob(join(self.path, '*', '*'))
-        self.filename = name + '.json'
-        codelist_files = list(filter(
-            lambda x: x.split('/')[-1] == self.filename, files))
-        if codelist_files == []:
+class Codelist(UserDict):
+    def __init__(self, slug, path, version):
+        self.slug = slug
+        self.version = version
+        self.filepath = join(path, slug + '.json')
+        if not exists(self.filepath):
             raise Exception('Codelist not found')
-        self.versions = [x.split('/')[2] for x in codelist_files]
-        self._codelist = {}
 
     def __iter__(self):
-        for k, v in self.all().items():
-            yield (k, v)
+        for k in self.all().keys():
+            yield k
 
-    def _load(self, version='latest'):
-        if version == 'latest':
-            version = max(self.versions)
-        with open(join(self.path, version, self.filename)) as f:
+    def __len__(self):
+        return len(self.all())
+
+    def __repr__(self):
+        return '<{} ({} v{})>'.format(
+            self.__class__.__name__,
+            self.slug,
+            self.version)
+
+    def _load(self):
+        if hasattr(self, 'data'):
+            return
+        with open(self.filepath) as f:
             j = json.load(f)
-        self._codelist[version] = {x['code']: x['name'] for x in j['data']}
         self._url = j['metadata']['url']
         self._name = j['metadata']['name']
         self._description = j['metadata']['description']
         self._complete = j['attributes']['complete']
+        self.data = {x['code']: x['name'] for x in j['data']}
 
-    def all(self, version='latest'):
-        if version == 'latest':
-            version = max(self.versions)
-        if version not in self._codelist:
-            self._load(version)
-        return self._codelist[version]
+    @property
+    def url(self):
+        self._load()
+        return self._url
 
-    def get(self, code, version='latest'):
-        return self.all(version).get(code)
+    @property
+    def name(self):
+        self._load()
+        return self._name
+
+    @property
+    def description(self):
+        self._load()
+        return self._description
+
+    @property
+    def complete(self):
+        self._load()
+        return self._complete
+
+    def all(self):
+        self._load()
+        return self.data
+
+    def get(self, code):
+        return self.all().get(code)
+
+    def __getitem__(self, code):
+        return self.all()[code]
 
 
-class CodelistSetMeta(type):
-    def __getattr__(cls, codelist_name):
-        return Codelist(codelist_name)
+def get(slug, version='latest', path=None):
+    if path is None:
+        path = join('__pyandicache__', 'codelists')
+    if version == 'latest':
+        major = max(listdir(path))
+        path = join(path, major)
+    else:
+        major = version.split('.')[0]
+        path = join(path, major)
+    return Codelist(slug, path, major)
 
 
-class CodelistSet(metaclass=CodelistSetMeta):
-    pass
+def all(version='latest', path=None):
+    if path is None:
+        path = join('__pyandicache__', 'codelists')
+    if version == 'latest':
+        major = max(listdir(path))
+        path = join(path, major)
+    else:
+        major = version.split('.')[0]
+        path = join(path, major)
+    return [Codelist(splitext(slug)[0], path, major) for slug in listdir(path)]
