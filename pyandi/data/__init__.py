7fbcf7f0f93ec4d158aca6cf503af345f55216e2
Andy Lulham <a.lulham@gmail.com>
2018-12-06 18:41:43 +0000

    Add a pyandi.datasets() function

--- pyandi/data/__init__.py
+++ pyandi/data/__init__.py
@@ -1,5 +1,15 @@
+from os.path import join
+
 from .registry import Registry
+from .dataset import DatasetSet
 
 
 def publishers():
     return Registry().publishers()
+
+
+def datasets():
+    r = Registry().publishers()
+    data_path = join(r.data_path, '*')
+    metadata_path = join(r.data_path, '*')
+    return DatasetSet(data_path, metadata_path)
