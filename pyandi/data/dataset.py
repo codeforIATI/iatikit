ad5f5d57def088737bb6c60445af19122dffece0
Andy Lulham <a.lulham@gmail.com>
2018-12-07 11:40:22 +0000

    Remove __str__ for dataset

--- pyandi/data/dataset.py
+++ pyandi/data/dataset.py
@@ -51,9 +51,6 @@ class Dataset:
     def __repr__(self):
         return '<{} ({})>'.format(self.__class__.__name__, self.name)
 
-    def __str__(self):
-        return self.name
-
     def is_valid(self):
         # TODO: This currently just checks for a valid root node
         try:
