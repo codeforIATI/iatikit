ed17765f71d1eb4288f0778deb135bc99b441369
Andy Lulham <a.lulham@gmail.com>
2018-12-07 00:08:01 +0000

    Make a start on the schema helper

--- pyandi/standard/schema.py
+++ pyandi/standard/schema.py
@@ -1,33 +1,35 @@
-class ActivitySchema:
+from os.path import join
+
+import xmlschema
+
+
+class Schema:
+    def __init__(self, version, path=None):
+        if path:
+            self.path = path
+        else:
+            self.path = join('__pyandicache__', 'standard', 'schemas')
+        self._schema = None
+
+    @property
+    def schema(self):
+        if not self._schema:
+            self._schema = xmlschema.XMLSchema(self.schema_path)
+        return self._schema
+
+    def find(self, search):
+        return self.schema.find(search)
+
+
+class OrganisationSchema(Schema):
     def __init__(self, version):
-        self._version = version
-
-    def get_query(self, shortcut, operator=None, value=None):
-        # TODO
-        res = self.getattr(shortcut)
-
-    @property
-    def title(self):
-        if self._version.startswith('1'):
-            xpath = 'title/text()'
-        else:
-            xpath = 'title/narrative/text()'
-        return {
-            'xpath': xpath,
-            'type': StringType,
-        }
-
-    @property
-    def description(self):
-        if self._version.startswith('1'):
-            xpath = 'description/text()'
-        else:
-            xpath = 'description/narrative/text()'
-        return {
-            'xpath': xpath,
-            'type': StringType,
-        }
+        super().__init__(version)
+        self.schema_path = join(self.path, version,
+                                'iati-organisations-schema.xsd')
 
 
-class StringType:
-    pass
+class ActivitySchema(Schema):
+    def __init__(self, version):
+        super().__init__(version)
+        self.schema_path = join(self.path, version,
+                                'iati-activities-schema.xsd')
