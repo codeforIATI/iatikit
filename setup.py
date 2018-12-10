67a84e8356004dd43497ac065261322fd24a6fc4
Andy Lulham <a.lulham@gmail.com>
2018-12-10 13:57:56 +0000

    v1.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/andylolz/pyandi',
     author='Andy Lulham',
     author_email='a.lulham@gmail.com',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     license='MIT',
     keywords='IATI',
