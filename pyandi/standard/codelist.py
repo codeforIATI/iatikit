e4c3279da2ad3889ba124dcce0e03eb4cdfeab5b
Andy Lulham <a.lulham@gmail.com>
2018-12-21 15:03:53 +0000

    Add codelist category filter

--- pyandi/standard/codelist.py
+++ pyandi/standard/codelist.py
@@ -50,7 +50,7 @@ class Codelist(GenericSet):
     def __init__(self, slug, path, version, **kwargs):
         super().__init__()
         self._key = 'code'
-        self._filters = ['code', 'version']
+        self._filters = ['code', 'version', 'category']
         self._wheres = kwargs
         self._instance_class = CodelistItem
 
@@ -78,13 +78,18 @@ class Codelist(GenericSet):
 
     def __iter__(self):
         code = self._wheres.get('code')
+        category = self._wheres.get('category')
         version = self._wheres.get('version', self.version)
         if version is not None:
             version = str(version)
         if code is not None:
             code = str(code)
+        if category is not None:
+            category = str(category)
         for data in self.data.values():
             if code is not None and data['code'] != code:
+                continue
+            if category is not None and data['category'] != category:
                 continue
             if version is not None:
                 version_from = data.get('from')
@@ -121,7 +126,7 @@ class Codelist(GenericSet):
 
 class CodelistItem:
     def __init__(self, codelist, **kwargs):
-        self._category = kwargs.get('category')
+        self.category = kwargs.get('category')
         self.status = kwargs.get('status', 'active')
         self.code = kwargs.get('code')
         self.name = kwargs.get('name')
