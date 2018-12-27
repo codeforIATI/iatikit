8f99421e23ccf928c2141d4146372444f046d5da
Andy Lulham <a.lulham@gmail.com>
2018-12-27 16:34:10 +0000

    Refactor registry.last_updated a bit

--- pyandi/data/registry.py
+++ pyandi/data/registry.py
@@ -12,37 +12,37 @@ from ..utils import download
 
 class Registry(object):
     def __init__(self, path=None):
+        self._last_updated = None
         if path:
             self.path = path
         else:
             self.path = join('__pyandicache__', 'registry')
 
-        if self.last_updated:
-            days_ago = (datetime.now() - self.last_updated).days
-            if days_ago > 7:
-                warning_msg = 'Warning: Data was last updated {} days ' + \
-                              'ago. Consider downloading a fresh ' + \
-                              'data dump, using:\n\n   ' + \
-                              '>>> pyandi.download.data()\n'
-                warnings.warn(warning_msg.format(days_ago))
-        else:
-            error_msg = 'Error: No data found! ' + \
-                          'Download a fresh data dump ' + \
-                          'using:\n\n   ' + \
+        last_updated = self.last_updated
+        days_ago = (datetime.now() - last_updated).days
+        if days_ago > 7:
+            warning_msg = 'Warning: Data was last updated {} days ' + \
+                          'ago. Consider downloading a fresh ' + \
+                          'data dump, using:\n\n   ' + \
                           '>>> pyandi.download.data()\n'
-            raise NoDataError(error_msg)
+            warnings.warn(warning_msg.format(days_ago))
 
     @property
     def last_updated(self):
-        try:
-            with open(join(self.path, 'metadata.json')) as f:
-                j = json.load(f)
+        if not self._last_updated:
+            try:
+                with open(join(self.path, 'metadata.json')) as f:
+                    j = json.load(f)
+            except FileNotFoundError:
+                error_msg = 'Error: No data found! ' + \
+                            'Download a fresh data dump ' + \
+                            'using:\n\n   ' + \
+                            '>>> pyandi.download.data()\n'
+                raise NoDataError(error_msg)
             last_updated = j['updated_at']
-            last_updated = datetime.strptime(
+            self._last_updated = datetime.strptime(
                 last_updated, '%Y-%m-%dT%H:%M:%SZ')
-        except:
-            last_updated = None
-        return last_updated
+        return self._last_updated
 
     @property
     def publishers(self):
