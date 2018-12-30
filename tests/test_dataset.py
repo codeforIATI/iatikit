7cc4d779b2fe275820c84cd6d218b557cd96aa07
Andy Lulham <a.lulham@gmail.com>
2018-12-30 09:25:27 +0000

    More DatasetSet tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,6 +1,8 @@
 from os.path import abspath, dirname, join
 from unittest import TestCase
 
+import pytest
+
 from pyandi.data.publisher import DatasetSet
 
 
@@ -23,3 +25,28 @@ class TestDatasets(TestCase):
     def test_datasets_filter_by_filetype(self):
         act_datasets = self.org_datasets.where(filetype='activity').all()
         assert len(act_datasets) == 1
+        assert act_datasets[0].name == 'fixture-org-activities'
+
+    def test_datasets_filter_by_name(self):
+        org_datasets = self.org_datasets.where(name='fixture-org-org').all()
+        assert len(org_datasets) == 1
+        assert org_datasets[0].name == 'fixture-org-org'
+
+    def test_datasets_get(self):
+        org_dataset = self.org_datasets.get('fixture-org-org')
+        assert org_dataset.name == 'fixture-org-org'
+
+    def test_datasets_get_unknown(self):
+        unknown_dataset = self.org_datasets.get('unknown')
+        assert unknown_dataset is None
+
+    def test_datasets_find(self):
+        org_dataset = self.org_datasets.find(name='fixture-org-org')
+        assert org_dataset.name == 'fixture-org-org'
+
+    def test_datasets_not_found(self):
+        with pytest.raises(IndexError):
+            self.org_datasets.find(name='not-a-dataset')
+
+    def test_datasets_count(self):
+        assert self.org_datasets.count() == 2
