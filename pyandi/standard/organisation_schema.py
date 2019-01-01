07f39ad8ecf205605f85cb2fb9b6e3dd7efdcdfe
Andy Lulham <a.lulham@gmail.com>
2019-01-01 21:38:32 +0000

    Comment out org-related code for now

--- pyandi/standard/organisation_schema.py
+++ pyandi/standard/organisation_schema.py
@@ -1,70 +1,70 @@
-from ..utils.exceptions import SchemaError
-from ..utils.types import StringType
+# from ..utils.exceptions import SchemaError
+# from ..utils.types import StringType
 
 
-class OrganisationSchema101(object):
-    def __init__(self):
-        self.version = '1.01'
+# class OrganisationSchema101(object):
+#     def __init__(self):
+#         self.version = '1.01'
 
-    def org_identifier(self):
-        return StringType('iati-identifier/text()')
-
-
-class OrganisationSchema102(OrganisationSchema101):
-    def __init__(self):
-        self.version = '1.02'
+#     def org_identifier(self):
+#         return StringType('iati-identifier/text()')
 
 
-class OrganisationSchema103(OrganisationSchema102):
-    def __init__(self):
-        self.version = '1.03'
+# class OrganisationSchema102(OrganisationSchema101):
+#     def __init__(self):
+#         self.version = '1.02'
 
 
-class OrganisationSchema104(OrganisationSchema103):
-    def __init__(self):
-        self.version = '1.04'
+# class OrganisationSchema103(OrganisationSchema102):
+#     def __init__(self):
+#         self.version = '1.03'
 
 
-class OrganisationSchema105(OrganisationSchema104):
-    def __init__(self):
-        self.version = '1.05'
+# class OrganisationSchema104(OrganisationSchema103):
+#     def __init__(self):
+#         self.version = '1.04'
 
 
-class OrganisationSchema201(OrganisationSchema105):
-    def __init__(self):
-        self.version = '2.01'
-
-    def org_identifier(self):
-        return StringType('organisation-identifier/text()')
+# class OrganisationSchema105(OrganisationSchema104):
+#     def __init__(self):
+#         self.version = '1.05'
 
 
-class OrganisationSchema202(OrganisationSchema201):
-    def __init__(self):
-        self.version = '2.02'
+# class OrganisationSchema201(OrganisationSchema105):
+#     def __init__(self):
+#         self.version = '2.01'
+
+#     def org_identifier(self):
+#         return StringType('organisation-identifier/text()')
 
 
-class OrganisationSchema203(OrganisationSchema202):
-    def __init__(self):
-        self.version = '2.03'
+# class OrganisationSchema202(OrganisationSchema201):
+#     def __init__(self):
+#         self.version = '2.02'
 
 
-def get_organisation_schema(version):
-    if version == '2.03':
-        return OrganisationSchema203()
-    elif version == '2.02':
-        return OrganisationSchema202()
-    elif version == '2.01':
-        return OrganisationSchema201()
-    elif version == '1.05':
-        return OrganisationSchema105()
-    elif version == '1.04':
-        return OrganisationSchema104()
-    elif version == '1.03':
-        return OrganisationSchema103()
-    elif version == '1.02':
-        return OrganisationSchema102()
-    elif version == '1.01':
-        return OrganisationSchema101()
+# class OrganisationSchema203(OrganisationSchema202):
+#     def __init__(self):
+#         self.version = '2.03'
 
-    msg = 'Unknown organisation schema: version: {}'.format(version)
-    raise SchemaError(msg)
+
+# def get_organisation_schema(version):
+#     if version == '2.03':
+#         return OrganisationSchema203()
+#     elif version == '2.02':
+#         return OrganisationSchema202()
+#     elif version == '2.01':
+#         return OrganisationSchema201()
+#     elif version == '1.05':
+#         return OrganisationSchema105()
+#     elif version == '1.04':
+#         return OrganisationSchema104()
+#     elif version == '1.03':
+#         return OrganisationSchema103()
+#     elif version == '1.02':
+#         return OrganisationSchema102()
+#     elif version == '1.01':
+#         return OrganisationSchema101()
+
+#     msg = 'Unknown organisation schema: version: {}'.format(version)
+#     raise SchemaError(msg)
