d67f73f4acd78ea2c582d722c8021533be9853de
Andy Lulham <a.lulham@gmail.com>
2019-01-02 12:56:47 +0000

    v1.5.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://pyandi.readthedocs.io',
     author='Andy Lulham',
     author_email='a.lulham@gmail.com',
-    version='1.5.2',
+    version='1.5.3',
     packages=find_packages(),
     license='MIT',
     keywords='IATI',
