2aad3bcf17a8693f01c8010509158836a9ba8a46
Andy Lulham <a.lulham@gmail.com>
2019-01-03 15:35:14 +0000

    Add some CodelistSet tests

--- tests/test_codelist.py
+++ tests/test_codelist.py
@@ -1,4 +1,4 @@
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
 import shutil
 import tempfile
 from unittest import TestCase
@@ -19,3 +19,31 @@ class TestNoCodelists(TestCase):
 
     def tearDown(self):
         shutil.rmtree(self.empty_path, ignore_errors=True)
+
+
+class TestCodelistSet(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestCodelistSet, self).__init__(*args, **kwargs)
+        codelist_path = join(dirname(abspath(__file__)),
+                             'fixtures', 'codelists')
+        self.codelists = CodelistSet(codelist_path)
+
+    def test_codelists_iter(self):
+        codelist_slugs = [
+            'Sector',
+            'SectorVocabulary',
+            'Vocabulary',
+        ]
+        assert len([x for x in self.codelists]) == 3
+        for x in self.codelists:
+            assert x.slug in codelist_slugs
+
+    def test_codelists_filter_version(self):
+        codelist_slugs = [
+            'Sector',
+            'Vocabulary',
+        ]
+        codelists = [x for x in self.codelists.where(version='1.01')]
+        assert len(codelists) == 2
+        for x in codelists:
+            assert x.slug in codelist_slugs
