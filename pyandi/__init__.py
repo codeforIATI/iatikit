f75b7350a7eb17901a7341364b45b1e9fbe5c066
Andy Lulham <a.lulham@gmail.com>
2019-01-08 11:17:05 +0000

    More reshuffling

--- pyandi/__init__.py
+++ pyandi/__init__.py
@@ -1,8 +1,13 @@
-from .data.registry import data  # noqa: F401
 from .standard.codelist import codelists  # noqa: F401
+from .data.registry import Registry
 from .data.publisher import Publisher  # noqa: F401
 from .data.dataset import Dataset  # noqa: F401
 from .data.activity import Activity  # noqa: F401
 from .data.sector import Sector  # noqa: F401
 from .utils import download  # noqa: F401
 from .__version__ import __version__  # noqa: F401
+
+
+def data(path=None):
+    """Helper function for constructing a Registry object."""
+    return Registry(path)
--- pyandi/data/registry.py
+++ pyandi/data/registry.py
@@ -81,8 +81,3 @@ class Registry(object):
     def download(self):
         """Download all IATI data and registry metadata."""
         return download.data(self.path)
-
-
-def data(path=None):
-    """Helper function for constructing a Registry object."""
-    return Registry(path)
