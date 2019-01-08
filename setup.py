b9e8615e955292363a04421854e782b546f8f94e
Andy Lulham <a.lulham@gmail.com>
2019-01-08 09:57:34 +0000

    Remove support for python 3.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     license='MIT',
     keywords='IATI',
     long_description=readme,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
         'lxml',
         'requests',
