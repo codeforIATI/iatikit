f8c58b6427796deaa9a335df563dbd00b05cc216
Andy Lulham <a.lulham@gmail.com>
2019-01-09 11:27:30 +0000

    Don’t instantiate schemas

--- pyandi/standard/activity_schema.py
+++ pyandi/standard/activity_schema.py
@@ -4,126 +4,136 @@ from ..utils.abstract import GenericType
 
 
 class ActivitySchema101(object):
-    def __init__(self):
-        self.version = '1.01'
+    version = '1.01'
 
-    def xpath(self):
+    @classmethod
+    def xpath(cls):
         return XPathType('')
 
-    def id(self):  # pylint: disable=invalid-name
-        return self.iati_identifier()
+    @classmethod
+    def id(cls):  # pylint: disable=invalid-name
+        return cls.iati_identifier()
 
-    def iati_identifier(self):
+    @classmethod
+    def iati_identifier(cls):
         return StringType('iati-identifier/text()')
 
-    def title(self):
+    @classmethod
+    def title(cls):
         return StringType('title/text()')
 
-    def description(self):
+    @classmethod
+    def description(cls):
         return StringType('description/text()')
 
-    def location(self):
+    @classmethod
+    def location(cls):
         return GenericType('location')
 
-    def sector(self):
+    @classmethod
+    def sector(cls):
         # TODO: This should include lookups for other v1.0x vocabs
         condition = {
             '1': [None, 'DAC'],
             '2': 'DAC-3',
         }
-        return SectorType('sector', self.version, condition)
+        return SectorType('sector', cls.version, condition)
 
-    def planned_start(self):
+    @classmethod
+    def planned_start(cls):
         return DateType('activity-date[@type="start-planned"]/@iso-date')
 
-    def actual_start(self):
+    @classmethod
+    def actual_start(cls):
         return DateType('activity-date[@type="start-actual"]/@iso-date')
 
-    def planned_end(self):
+    @classmethod
+    def planned_end(cls):
         return DateType('activity-date[@type="end-planned"]/@iso-date')
 
-    def actual_end(self):
+    @classmethod
+    def actual_end(cls):
         return DateType('activity-date[@type="end-actual"]/@iso-date')
 
 
 class ActivitySchema102(ActivitySchema101):
-    def __init__(self):
-        self.version = '1.02'
+    version = '1.02'
 
 
 class ActivitySchema103(ActivitySchema102):
-    def __init__(self):
-        self.version = '1.03'
+    version = '1.03'
 
 
 class ActivitySchema104(ActivitySchema103):
-    def __init__(self):
-        self.version = '1.04'
+    version = '1.04'
 
 
 class ActivitySchema105(ActivitySchema104):
-    def __init__(self):
-        self.version = '1.05'
+    version = '1.05'
 
 
 class ActivitySchema201(ActivitySchema105):
-    def __init__(self):
-        self.version = '2.01'
+    version = '2.01'
 
-    def title(self):
+    @classmethod
+    def title(cls):
         return StringType('title/narrative/text()')
 
-    def description(self):
+    @classmethod
+    def description(cls):
         return StringType('description/narrative/text()')
 
-    def sector(self):
+    @classmethod
+    def sector(cls):
         condition = {
             '1': [None, '1'],
         }
-        return SectorType('sector', self.version, condition)
+        return SectorType('sector', cls.version, condition)
 
-    def planned_start(self):
+    @classmethod
+    def planned_start(cls):
         return DateType('activity-date[@type="1"]/@iso-date')
 
-    def actual_start(self):
+    @classmethod
+    def actual_start(cls):
         return DateType('activity-date[@type="2"]/@iso-date')
 
-    def planned_end(self):
+    @classmethod
+    def planned_end(cls):
         return DateType('activity-date[@type="3"]/@iso-date')
 
-    def actual_end(self):
+    @classmethod
+    def actual_end(cls):
         return DateType('activity-date[@type="4"]/@iso-date')
 
 
 class ActivitySchema202(ActivitySchema201):
-    def __init__(self):
-        self.version = '2.02'
+    version = '2.02'
 
 
 class ActivitySchema203(ActivitySchema202):
-    def __init__(self):
-        self.version = '2.03'
+    version = '2.03'
 
 
 def get_activity_schema(version):
     schema = None
 
     if version == '2.03':
-        schema = ActivitySchema203()
+        schema = ActivitySchema203
     elif version == '2.02':
-        schema = ActivitySchema202()
+        schema = ActivitySchema202
     elif version == '2.01':
-        schema = ActivitySchema201()
+        schema = ActivitySchema201
     elif version == '1.05':
-        schema = ActivitySchema105()
+        schema = ActivitySchema105
     elif version == '1.04':
-        schema = ActivitySchema104()
+        schema = ActivitySchema104
     elif version == '1.03':
-        schema = ActivitySchema103()
+        schema = ActivitySchema103
     elif version == '1.02':
-        schema = ActivitySchema102()
+        schema = ActivitySchema102
     elif version == '1.01':
-        schema = ActivitySchema101()
+        schema = ActivitySchema101
 
     if not schema:
         msg = 'Unknown activity schema: version: {}'.format(version)
--- pyandi/standard/organisation_schema.py
+++ pyandi/standard/organisation_schema.py
@@ -3,68 +3,62 @@
 
 
 # class OrganisationSchema101(object):
-#     def __init__(self):
-#         self.version = '1.01'
+#     version = '1.01'
 
-#     def org_identifier(self):
+#     @classmethod
+#     def org_identifier(cls):
 #         return StringType('iati-identifier/text()')
 
 
 # class OrganisationSchema102(OrganisationSchema101):
-#     def __init__(self):
-#         self.version = '1.02'
+#     version = '1.02'
 
 
 # class OrganisationSchema103(OrganisationSchema102):
-#     def __init__(self):
-#         self.version = '1.03'
+#     version = '1.03'
 
 
 # class OrganisationSchema104(OrganisationSchema103):
-#     def __init__(self):
-#         self.version = '1.04'
+#     version = '1.04'
 
 
 # class OrganisationSchema105(OrganisationSchema104):
-#     def __init__(self):
-#         self.version = '1.05'
+#     version = '1.05'
 
 
 # class OrganisationSchema201(OrganisationSchema105):
-#     def __init__(self):
-#         self.version = '2.01'
+#     version = '2.01'
 
-#     def org_identifier(self):
+#     @classmethod
+#     def org_identifier(cls):
 #         return StringType('organisation-identifier/text()')
 
 
 # class OrganisationSchema202(OrganisationSchema201):
-#     def __init__(self):
-#         self.version = '2.02'
+#     version = '2.02'
 
 
 # class OrganisationSchema203(OrganisationSchema202):
-#     def __init__(self):
-#         self.version = '2.03'
+#     version = '2.03'
 
 
 # def get_organisation_schema(version):
 #     if version == '2.03':
-#         return OrganisationSchema203()
+#         return OrganisationSchema203
 #     elif version == '2.02':
-#         return OrganisationSchema202()
+#         return OrganisationSchema202
 #     elif version == '2.01':
-#         return OrganisationSchema201()
+#         return OrganisationSchema201
 #     elif version == '1.05':
-#         return OrganisationSchema105()
+#         return OrganisationSchema105
 #     elif version == '1.04':
-#         return OrganisationSchema104()
+#         return OrganisationSchema104
 #     elif version == '1.03':
-#         return OrganisationSchema103()
+#         return OrganisationSchema103
 #     elif version == '1.02':
-#         return OrganisationSchema102()
+#         return OrganisationSchema102
 #     elif version == '1.01':
-#         return OrganisationSchema101()
+#         return OrganisationSchema101
 
 #     msg = 'Unknown organisation schema: version: {}'.format(version)
 #     raise SchemaError(msg)
