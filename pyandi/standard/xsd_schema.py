e8f69a3b7a2e2baa452ca908728e1a1db6f56baa
Andy Lulham <a.lulham@gmail.com>
2019-01-16 15:44:06 +0000

    Construct a new XMLSchema for each validation

--- pyandi/standard/xsd_schema.py
+++ pyandi/standard/xsd_schema.py
@@ -18,21 +18,20 @@ class XSDSchema(object):
         }.get(filetype)
 
         version_path = version.replace('.', '')
-        schema_path = join(CONFIG['paths']['standard'], 'schemas',
-                           version_path, schema)
+        self.schema_path = join(CONFIG['paths']['standard'], 'schemas',
+                                version_path, schema)
 
-        if not exists(schema_path):
+        if not exists(self.schema_path):
             msg = 'No {filetype} schema found for IATI version "{version}".'
             msg = msg.format(filetype=filetype, version=version)
             raise SchemaNotFoundError(msg)
-
-        self.schema = etree.XMLSchema(etree.parse(schema_path))
 
     def __repr__(self):
         return '<{} ({} {})>'.format(self.__class__.__name__,
                                      self.filetype, self.version)
 
     def validate(self, dataset):
+        schema = etree.XMLSchema(etree.parse(self.schema_path))
         is_valid = self.schema.validate(dataset.etree)
         error_log = ['{}: {}'.format(e.type_name, e.message)
                      for e in self.schema.error_log]
