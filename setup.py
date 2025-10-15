4bac9863c7b414f7b42ad6da171848ecec5384b4
Andy Lulham <a.lulham@gmail.com>
2025-10-15 23:09:33 +0100

    chore: linting

--- setup.py
+++ setup.py
@@ -1,48 +1,48 @@
-from setuptools import setup, find_packages
 from os.path import abspath, dirname, join
 
+from setuptools import find_packages, setup
 
 path = abspath(dirname(__file__))
-with open(join(path, 'README.md')) as f:
+with open(join(path, "README.md")) as f:
     readme = f.read()
 
 data = {}
-with open(join(path, 'iatikit', '__version__.py')) as f:
+with open(join(path, "iatikit", "__version__.py")) as f:
     exec(f.read(), data)
 
 setup(
-    name='iatikit',
-    description='A toolkit for using IATI data.',
-    url='https://iatikit.readthedocs.io',
-    author='Andy Lulham',
-    author_email='a.lulham@gmail.com',
-    version=data.get('__version__'),
+    name="iatikit",
+    description="A toolkit for using IATI data.",
+    url="https://iatikit.readthedocs.io",
+    author="Andy Lulham",
+    author_email="a.lulham@gmail.com",
+    version=data.get("__version__"),
     packages=find_packages(),
-    license='MIT',
-    keywords='IATI',
+    license="MIT",
+    keywords="IATI",
     long_description=readme,
     long_description_content_type="text/markdown",
     install_requires=[
-        'configparser',
-        'lxml',
-        'requests',
+        "configparser",
+        "lxml",
+        "requests",
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
 )
